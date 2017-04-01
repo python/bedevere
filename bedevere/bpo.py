c9ddc5891d7b3d44bf00f25975f7417af762e942
Brett Cannon <brett@python.org>
2017-04-01 13:26:37 -0700

    Consider a PR w/ a "trivial" label as passing

--- bedevere/bpo.py
+++ bedevere/bpo.py
@@ -5,30 +5,43 @@ from . import router
 
 ISSUE_RE = re.compile(r"bpo-(?P<issue>\d+)")
 STATUS_TEMPLATE = {"context": "bedevere/issue-number"}
-FAILURE_STATUS = {
-    "state": "failure",
-    "target_url": "https://cpython-devguide.readthedocs.io/pullrequest.html?highlight=bpo-#submitting",
-    "description": """No issue number of the form "bpo-NNNN" found in the pull request's title.""",
-}
+FAILURE_STATUS = STATUS_TEMPLATE.copy()
+FAILURE_STATUS["state"] = "failure"
+FAILURE_STATUS["target_url"] = "https://cpython-devguide.readthedocs.io/pullrequest.html?highlight=bpo-#submitting"
+FAILURE_STATUS["description"] = """No issue number of the form "bpo-NNNN" found in the pull request's title."""
+TRIVIAL_LABEL = "trivial"
+TRIVIAL_STATUS = STATUS_TEMPLATE.copy()
+TRIVIAL_STATUS["state"] = "success"
+TRIVIAL_STATUS["description"] = "No issue number necessary."
+
+
+async def _post_status(gh, event, status):
+    """Post a status in reaction to an event."""
+    sha = event.data["pull_request"]["head"]["sha"]
+    await gh.post(f"/repos/python/cpython/status/{sha}", data=status)
 
 
 @router.route("pull_request", "opened")
 @router.route("pull_request", "synchronize")
 async def set_status(gh, event):
     """Set the issue number status on the pull request."""
-    status = STATUS_TEMPLATE.copy()
-    sha = event.data["pull_request"]["head"]["sha"]
     issue_number_match = ISSUE_RE.match(event.data["pull_request"]["title"])
     if not issue_number_match:
-        # XXX check for a "trivial" label.
-        #     If the even is pull_request/labeled, check if the label is "trivial".
-        status.update(FAILURE_STATUS)
+        issue_url = event.data["pull_request"]["issue_url"]
+        data = await gh.getitem(issue_url)
+        for label in data["labels"]:
+            if label["name"] == TRIVIAL_LABEL:
+                status = TRIVIAL_STATUS
+                break
+        else:
+            status = FAILURE_STATUS
     else:
+        status = STATUS_TEMPLATE.copy()
         status["state"] = "success"
         issue_number = issue_number_match.group("issue")
         status["description"] = f"Issue number {issue_number} found."
         status["target_url"] = f"https://bugs.python.org/issue{issue_number}"
-    await gh.post(f"/repos/python/cpython/status/{sha}", data=status)
+    await _post_status(gh, event, status)
 
 
 @router.route("pull_request", "edited")
@@ -39,4 +52,15 @@ async def title_edited(gh, event):
     await set_status(gh, event)
 
 
-# XXX @router.route("pull_request", "labeled")
+@router.route("pull_request", "labeled")
+async def new_label(gh, event):
+    """Update the status if the "trivial" label was added."""
+    if event.data["label"]["name"] == TRIVIAL_LABEL:
+        await _post_status(gh, event, TRIVIAL_STATUS)
+
+
+@router.route("pull_request", "unlabeled")
+async def removed_label(gh, event):
+    """Re-check the status if the "trivial" label is removed."""
+    if event.data["label"]["name"] == TRIVIAL_LABEL:
+        await set_status(gh, event)
--- tests/test_bpo.py
+++ tests/test_bpo.py
@@ -7,21 +7,35 @@ from bedevere import bpo
 
 
 class FakeGH:
+
+    def __init__(self, *, getitem=None):
+        self._getitem_return = getitem
+
+    async def getitem(self, url):
+        return self._getitem_return
+
     async def post(self, url, data):
+        self.url = url
         self.data = data
 
 
 @pytest.mark.asyncio
-async def test_status_failure():
+async def test_set_status_failure():
     data = {
         "pull_request":
             {
                 "head": {"sha": "git-sha"},
                 "title": "No issue in title",
+                "issue_url": "issue URL",
             },
     }
+    issue_data = {
+        "labels": [
+            {"name": "non-trivial"},
+        ]
+    }
     event = sansio.Event(data, event="pull_request", delivery_id="12345")
-    gh = FakeGH()
+    gh = FakeGH(getitem=issue_data)
     await bpo.set_status(gh, event)
     status = gh.data
     assert status["state"] == "failure"
@@ -30,7 +44,7 @@ async def test_status_failure():
 
 
 @pytest.mark.asyncio
-async def test_status_success():
+async def test_set_status_success():
     data = {
         "pull_request":
             {
@@ -46,6 +60,31 @@ async def test_status_success():
     assert status["target_url"].endswith("issue1234")
     assert "1234" in status["description"]
     assert status["context"] == "bedevere/issue-number"
+    assert "git-sha" in gh.url
+
+
+@pytest.mark.asyncio
+async def test_set_status_success_via_trivial_label():
+    data = {
+        "pull_request":
+            {
+                "head": {"sha": "git-sha"},
+                "title": "No issue in title",
+                "issue_url": "issue URL",
+            },
+    }
+    issue_data = {
+        "labels": [
+            {"name": "trivial"},
+        ]
+    }
+    event = sansio.Event(data, event="pull_request", delivery_id="12345")
+    gh = FakeGH(getitem=issue_data)
+    await bpo.set_status(gh, event)
+    status = gh.data
+    assert status["state"] == "success"
+    assert status["context"] == "bedevere/issue-number"
+    assert "git-sha" in gh.url
 
 
 @pytest.mark.asyncio
@@ -62,4 +101,66 @@ async def test_edit_other_than_title():
     event = sansio.Event(data, event="pull_request", delivery_id="12345")
     gh = FakeGH()
     await bpo.title_edited(gh, event)
+    assert not hasattr(gh, "data")
+
+
+@pytest.mark.asyncio
+async def test_new_label_trivial():
+    data = {
+        "action": "labeled",
+        "label": {"name": "trivial"},
+        "pull_request": {"head": {"sha": "git-sha"}},
+    }
+    event = sansio.Event(data, event="pull_request", delivery_id="12345")
+    gh = FakeGH()
+    await bpo.new_label(gh, event)
+    assert gh.data["state"] == "success"
+    assert "git-sha" in gh.url
+
+
+@pytest.mark.asyncio
+async def test_new_label_not_trivial():
+    data = {
+        "action": "labeled",
+        "label": {"name": "non-trivial"},
+        "pull_request": {"head": {"sha": "git-sha"}},
+    }
+    event = sansio.Event(data, event="pull_request", delivery_id="12345")
+    gh = FakeGH()
+    await bpo.new_label(gh, event)
+    assert not hasattr(gh, "data")
+
+
+@pytest.mark.asyncio
+async def test_removed_label_trivial():
+    data = {
+        "action": "unlabeled",
+        "label": {"name": "non-trivial"},
+        "pull_request":
+            {
+                "head": {"sha": "git-sha"},
+                "title": "bpo-1234: an issue!",
+            },
+    }
+    event = sansio.Event(data, event="pull_request", delivery_id="12345")
+    gh = FakeGH()
+    await bpo.set_status(gh, event)
+    status = gh.data
+    assert status["state"] == "success"
+    assert status["target_url"].endswith("issue1234")
+    assert "1234" in status["description"]
+    assert status["context"] == "bedevere/issue-number"
+    assert "git-sha" in gh.url
+
+
+@pytest.mark.asyncio
+async def test_removed_label_non_trivial():
+    data = {
+        "action": "unlabeled",
+        "label": {"name": "non-trivial"},
+        "pull_request": {"head": {"sha": "git-sha"}},
+    }
+    event = sansio.Event(data, event="pull_request", delivery_id="12345")
+    gh = FakeGH()
+    await bpo.new_label(gh, event)
     assert not hasattr(gh, "data")

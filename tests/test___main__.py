89fd19169633d180c44ced9741eca49265b9b705
Brett Cannon <brett@python.org>
2017-04-02 12:07:05 -0700

    Add a test for the ping

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -4,6 +4,17 @@ import pytest
 from bedevere import __main__ as main
 
 
+async def test_ping(test_client):
+    app = web.Application()
+    app.router.add_post("/", main.main)
+    client = await test_client(app)
+    headers = {"x-github-event": "ping",
+               "x-github-delivery": "1234"}
+    data = {"zen": "testing is good"}
+    response = await client.post("/", headers=headers, json=data)
+    assert response.status == 200
+
+
 async def test_success(test_client):
     app = web.Application()
     app.router.add_post("/", main.main)

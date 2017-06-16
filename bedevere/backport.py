98f7de09ef6145a0452f7e17eed2a54d1db4d6de
Brett Cannon <brett@python.org>
2017-06-16 13:22:12 -0700

    Fix code to work with an event and not a pull request directly

--- bedevere/backport.py
+++ bedevere/backport.py
@@ -18,22 +18,23 @@ def normalize_title(title, body):
         return title
     else:
         # Being paranoid in case \r\n is used.
-        return title[:-1] + body[1:].partition('\n')[0].rstrip()
+        return title[:-1] + body[1:].partition('\r\n')[0]
 
 
 router.register('pull_request', action='opened')
 async def remove_backport_label(event, gh, *args, **kwargs):
-    title = normalize_title(event.data['title'], event.data['body'])
+    title = normalize_title(event.data['pull_request']['title'], event.data['pull_request']['body'])
     title_match = TITLE_RE.match(title)
     if title_match is None:
         return
     branch = title_match.group('branch')
     backport_label = BACKPORT_LABEL.format(branch=branch)
     pr_to_backport = title_match.group('pr')
+    backported_pr = event.data['number']
 
-    issue = await gh.getitem(event.data['issue_url'])
-    if backport_label not in {label['name'] for label in issue['labels']}:
+    issue_to_backport = await gh.getitem(event.data['repository']['issues_url'], {'number': pr_to_backport})
+    if not any(backport_label == label['name'] for label in issue_to_backport['labels']):
         return
-    await gh.delete(issue['labels_url'], {'name': backport_label})
-    message = MESSAGE_TEMPLATE.format(branch=branch, pr=pr_to_backport)
-    await gh.post(issue['comments_url'], data={'body': message})
+    await gh.delete(issue_to_backport['labels_url'], {'name': backport_label})
+    message = MESSAGE_TEMPLATE.format(branch=branch, pr=backported_pr)
+    await gh.post(issue_to_backport['comments_url'], data={'body': message})
--- tests/test_backport.py
+++ tests/test_backport.py
@@ -13,8 +13,8 @@ class FakeGH:
         self.delete_url = None
         self.post_url = self.post_data = None
 
-    async def getitem(self, url):
-        self.getitem_url = url
+    async def getitem(self, url, url_vars):
+        self.getitem_url = sansio.format_url(url, url_vars)
         return self._getitem_return
 
     async def delete(self, url, url_vars):
@@ -31,9 +31,7 @@ def test_title_normalization():
     assert backport.normalize_title(title, body) == title
 
     title = "[2.7] bpo-29243: Fix Makefile with respect to --enable-optimizations …"
-    body = """…(GH-1478)
-
-    stuff"""
+    body = "…(GH-1478)\r\n\r\nstuff"
     expected = '[2.7] bpo-29243: Fix Makefile with respect to --enable-optimizations (GH-1478)'
     assert backport.normalize_title(title, body) == expected
 
@@ -47,7 +45,10 @@ def test_title_normalization():
 
 
 async def test_missing_branch_in_title():
-    event = sansio.Event({'title': 'Backport this (GH-1234)', 'body': ''}, event='pull_request',
+    data = {
+        'pull_request': {'title': 'Backport this (GH-1234)', 'body': ''},
+    }
+    event = sansio.Event(data, event='pull_request',
                          delivery_id='1')
     gh = FakeGH()
     await backport.remove_backport_label(event, gh)
@@ -55,7 +56,10 @@ async def test_missing_branch_in_title():
 
 
 async def test_missing_pr_in_title():
-    event = sansio.Event({'title': '[3.6] Backport this', 'body': ''}, event='pull_request',
+    data = {
+        'pull_request': {'title': '[3.6] Backport this', 'body': ''},
+    }
+    event = sansio.Event(data, event='pull_request',
                         delivery_id='1')
     gh = FakeGH()
     await backport.remove_backport_label(event, gh)
@@ -65,36 +69,44 @@ async def test_missing_pr_in_title():
 async def test_missing_label():
     title = '[3.6] Backport this (GH-1234)'
     data = {
-        'title': title,
-        'body': '',
-        'issue_url': 'https://api.github.com/repos/python/cpython/issues/1234',
+        'number': 2248,
+        'pull_request': {
+            'title': title,
+            'body': '',
+        },
+        'repository': {'issues_url': 'https://api.github.com/repos/python/cpython/issues{/number}'}
     }
     event = sansio.Event(data, event='pull_request',
                         delivery_id='1')
     gh = FakeGH(getitem={'labels': [{'name': 'CLA signed'}]})
     await backport.remove_backport_label(event, gh)
-    assert gh.getitem_url == data['issue_url']
+    assert gh.getitem_url == sansio.format_url(data['repository']['issues_url'], {'number': '1234'})
     assert gh.delete_url is None
 
 
 async def test_success():
     event_data = {
-        'title': '[3.6] Backport this …',
-        'body': '…(GH-1234)',
-        'issue_url': 'https://api.github.com/repos/python/cpython/issues/2248',
+        'number': 2248,
+        'pull_request': {
+            'title': '[3.6] Backport this …',
+            'body': '…(GH-1234)',
+        },
+        'repository': {
+            'issues_url': 'https://api.github.com/repos/python/cpython/issues{/number}',
+        },
     }
     event = sansio.Event(event_data, event='pull_request',
                         delivery_id='1')
     issue_data = {
         'labels': [{'name': 'needs backport to 3.6'}],
-        'labels_url': 'https://api.github.com/repos/python/cpython/issues/2248/labels{/name}',
-        'comments_url': 'https://api.github.com/repos/python/cpython/issues/2248/comments',
+        'labels_url': 'https://api.github.com/repos/python/cpython/issues/1234/labels{/name}',
+        'comments_url': 'https://api.github.com/repos/python/cpython/issues/1234/comments',
     }
     gh = FakeGH(getitem=issue_data)
     await backport.remove_backport_label(event, gh)
-    assert gh.getitem_url == event_data['issue_url']
+    assert gh.getitem_url == sansio.format_url(event_data['repository']['issues_url'], {'number': '1234'})
     assert gh.delete_url == sansio.format_url(issue_data['labels_url'],
                                               {'name': 'needs backport to 3.6'})
     assert gh.post_url == issue_data['comments_url']
     message = gh.post_data['body']
-    assert message == backport.MESSAGE_TEMPLATE.format(branch='3.6', pr='1234')
+    assert message == backport.MESSAGE_TEMPLATE.format(branch='3.6', pr='2248')

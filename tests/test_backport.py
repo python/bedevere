2ac8d751bf487809230b427ca49fde099af7fca7
Brett Cannon <brett@python.org>
2017-09-29 15:41:54 -0700

    Test through the router

--- tests/test_backport.py
+++ tests/test_backport.py
@@ -13,7 +13,7 @@ class FakeGH:
         self.delete_url = None
         self.post_url = self.post_data = None
 
-    async def getitem(self, url, url_vars):
+    async def getitem(self, url, url_vars={}):
         self.getitem_url = sansio.format_url(url, url_vars)
         return self._getitem_return
 
@@ -27,51 +27,56 @@ class FakeGH:
 
 async def test_missing_branch_in_title():
     data = {
+        'action': 'opened',
         'pull_request': {'title': 'Backport this (GH-1234)', 'body': ''},
     }
     event = sansio.Event(data, event='pull_request',
                          delivery_id='1')
     gh = FakeGH()
-    await backport.remove_backport_label(event, gh)
+    await backport.router.dispatch(event, gh)
     assert gh.getitem_url is None
 
 
 async def test_missing_pr_in_title():
     data = {
+        'action': 'opened',
         'pull_request': {'title': '[3.6] Backport this', 'body': ''},
     }
     event = sansio.Event(data, event='pull_request',
                         delivery_id='1')
     gh = FakeGH()
-    await backport.remove_backport_label(event, gh)
+    await backport.router.dispatch(event, gh)
     assert gh.getitem_url is None
 
 
-async def test_missing_label():
+async def test_missing_backport_label():
     title = '[3.6] Backport this (GH-1234)'
     data = {
+        'action': 'opened',
         'number': 2248,
         'pull_request': {
             'title': title,
             'body': '',
+            'issue_url': 'https://api.github.com/issue/2248',
         },
         'repository': {'issues_url': 'https://api.github.com/repos/python/cpython/issues{/number}'}
     }
     event = sansio.Event(data, event='pull_request',
                         delivery_id='1')
     gh = FakeGH(getitem={'labels': [{'name': 'CLA signed'}]})
-    await backport.remove_backport_label(event, gh)
+    await backport.router.dispatch(event, gh)
     assert gh.getitem_url == sansio.format_url(data['repository']['issues_url'], {'number': '1234'})
     assert gh.delete_url is None
 
 
-async def test_success():
+async def test_backport_label_removal_success():
     event_data = {
         'action': 'opened',
         'number': 2248,
         'pull_request': {
             'title': '[3.6] Backport this …',
             'body': '…(GH-1234)',
+            'issue_url': 'https://api.github.com/issue/2248',
         },
         'repository': {
             'issues_url': 'https://api.github.com/repos/python/cpython/issues{/number}',

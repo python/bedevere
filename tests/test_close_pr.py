0047faae131df34cbc4c6a6aa10ff6b04c144bdc
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-09-28 19:20:05 -0700

    test close pr on action = "synchronize"

--- tests/test_close_pr.py
+++ tests/test_close_pr.py
@@ -19,9 +19,38 @@ class FakeGH:
 
 
 @pytest.mark.asyncio
-async def test_close_invalid_pr():
+async def test_close_invalid_pr_on_open():
     data = {
         "action": "opened",
+        "pull_request": {
+            "statuses_url": "https://api.github.com/blah/blah/git-sha",
+            "title": "No issue in title",
+            "issue_url": "issue URL",
+            "url": "https://api.github.com/org/repo/pulls/123",
+            "head": {
+                "label": "python:3.6"
+            },
+            "base": {
+                "label": "python:master"
+            }
+        },
+    }
+    pr_data = {
+        "labels": [
+            {"name": "non-trivial"},
+        ]
+    }
+    event = sansio.Event(data, event="pull_request", delivery_id="12345")
+    gh = FakeGH(getitem=pr_data)
+    await close_pr.router.dispatch(event, gh)
+    patch_data = gh.patch_data
+    assert patch_data["state"] == "closed"
+
+
+@pytest.mark.asyncio
+async def test_close_invalid_pr_on_open():
+    data = {
+        "action": "synchronize",
         "pull_request": {
             "statuses_url": "https://api.github.com/blah/blah/git-sha",
             "title": "No issue in title",

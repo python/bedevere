3aaf79d3aded98dea9bb12461c6ef050fd95d153
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-03-13 22:54:43 +0200

    Adopt to aiohttp 3.0 (#101)

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -4,10 +4,10 @@ import pytest
 from bedevere import __main__ as main
 
 
-async def test_ping(test_client):
+async def test_ping(aiohttp_client):
     app = web.Application()
     app.router.add_post("/", main.main)
-    client = await test_client(app)
+    client = await aiohttp_client(app)
     headers = {"x-github-event": "ping",
                "x-github-delivery": "1234"}
     data = {"zen": "testing is good"}
@@ -15,10 +15,10 @@ async def test_ping(test_client):
     assert response.status == 200
 
 
-async def test_success(test_client):
+async def test_success(aiohttp_client):
     app = web.Application()
     app.router.add_post("/", main.main)
-    client = await test_client(app)
+    client = await aiohttp_client(app)
     headers = {"x-github-event": "project",
                "x-github-delivery": "1234"}
     # Sending a payload that shouldn't trigger any networking, but no errors
@@ -28,11 +28,11 @@ async def test_success(test_client):
     assert response.status == 200
 
 
-async def test_failure(test_client):
+async def test_failure(aiohttp_client):
     """Even in the face of an exception, the server should not crash."""
     app = web.Application()
     app.router.add_post("/", main.main)
-    client = await test_client(app)
+    client = await aiohttp_client(app)
     # Missing key headers.
     response = await client.post("/", headers={})
     assert response.status == 500

fb7063f08a9c9338cd1cf4860f0d5ad0e7c59585
Lysandros Nikolaou <lisandrosnik@gmail.com>
2019-09-10 14:20:49 +0300

    Use f-string for `reviewers_url`
    Co-Authored-By: Brett Cannon <54418+brettcannon@users.noreply.github.com>

--- bedevere/stage.py
+++ bedevere/stage.py
@@ -223,7 +223,7 @@ async def new_comment(event, gh, *args, **kwargs):
         comment = ACK.format(greeting=thanks, core_devs=core_devs)
         await gh.post(issue["comments_url"], data={"body": comment})
         # Re-request reviews from core developers based on the new state of the PR.
-        reviewers_url = pr_url + '/requested_reviewers'
+        reviewers_url = f'{pr_url}/requested_reviewers'
         reviewers = [core_dev async for core_dev in core_dev_reviewers(gh, pr_url)]
         await gh.post(reviewers_url, data={"reviewers": reviewers})
 

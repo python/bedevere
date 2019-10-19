fa707ea6b9347ab83b75f66c74b26fc6bae75617
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-10-19 09:26:39 +0530

    Fix grammar
    Co-Authored-By: Brett Cannon <54418+brettcannon@users.noreply.github.com>

--- bedevere/stage.py
+++ bedevere/stage.py
@@ -25,7 +25,7 @@ digraph "PR stages" {
   "Awaiting review" -> "Awaiting core review" [label="New review by another contributor", color=blue]
   "Awaiting core review" -> "Awaiting core review" [label="New review by another contributor", color=blue]
   "Awaiting core review" -> "Awaiting changes" [label="New core review requests changes", color=green]
-  "Awaiting changes" -> "Awaiting change review" [label="Comments changes are done by contributor\nBedevere requests review from core-dev", color=orange]
+  "Awaiting changes" -> "Awaiting change review" [label="Changes are done by contributor\nBedevere requests review from core-dev", color=orange]
   "Awaiting change review" -> "Awaiting changes" [label="New core review requests changes", color=green]
   "Awaiting change review" -> "Awaiting merge" [label="New core review approves", color=green]
 

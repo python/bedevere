73788673a6d9b8574cf714b5799014e079896702
Jacob Walls <jacobtylerwalls@gmail.com>
2022-10-08 15:34:14 -0400

    Fix links to Lifecycle of a Pull Request: Submitting

--- bedevere/gh_issue.py
+++ bedevere/gh_issue.py
@@ -105,7 +105,7 @@ def create_failure_status_issue_not_present(issue_number: int, *, kind: IssueKin
 def create_failure_status_no_issue():
     """Create a failure status for when no issue number was found in the title."""
     description = 'No issue # in title or "skip issue" label found'
-    url = "https://devguide.python.org/pullrequest/#submitting"
+    url = "https://devguide.python.org/getting-started/pull-request-lifecycle.html#submitting"
     return util.create_status(STATUS_CONTEXT, util.StatusState.FAILURE,
                               description=description,
                               target_url=url)

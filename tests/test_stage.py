184c1745a1abeb650b65b15c6fc6b51228e66732
Itamar Ostricher <itamarost@gmail.com>
2023-05-01 16:15:18 -0700

    gh-554: Handle Draft PRs

--- tests/test_stage.py
+++ tests/test_stage.py
@@ -72,6 +72,46 @@ async def test_stage():
     assert post_[1] == [awaiting.Blocker.merge.value]
 
 
+async def test_opened_draft_pr():
+    # New Draft PR from a core dev.
+    username = "brettcannon"
+    issue_url = "https://api.github.com/issue/42"
+    data = {
+        "action": "opened",
+        "pull_request": {
+            "user": {
+                "login": username,
+            },
+            "issue_url": issue_url,
+            "draft": True,
+        },
+    }
+    event = sansio.Event(data, event="pull_request", delivery_id="12345")
+    teams = [{"name": "python core", "id": 6}]
+    items = {
+        f"https://api.github.com/teams/6/memberships/{username}": "OK",
+        issue_url: {"labels": [], "labels_url": "https://api.github.com/labels"},
+    }
+    gh = FakeGH(
+        getiter={"https://api.github.com/orgs/python/teams": teams}, getitem=items
+    )
+    await awaiting.router.dispatch(event, gh)
+    assert len(gh.post_) == 0
+
+    # Draft PR is published
+    data["action"] = "edited"
+    data["pull_request"]["draft"] = False
+    event = sansio.Event(data, event="pull_request", delivery_id="12345")
+    gh = FakeGH(
+        getiter={"https://api.github.com/orgs/python/teams": teams}, getitem=items
+    )
+    await awaiting.router.dispatch(event, gh)
+    assert len(gh.post_) == 1
+    post_ = gh.post_[0]
+    assert post_[0] == "https://api.github.com/labels"
+    assert post_[1] == [awaiting.Blocker.core_review.value]
+
+
 async def test_opened_pr():
     # New PR from a core dev.
     username = "brettcannon"

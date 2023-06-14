de3b48978ec505356124060e80fbbea5ecbe5d1d
Mariatta <mariatta@python.org>
2023-06-14 10:42:01 -0700

    Make bedevere works as a GitHub App
    Instead of using Oauth Token, use the GitHub App Installation Access Token.
    If the "installation" dict is passed in the webhook event, use it.
    
    Made some changes to accommodate testing in personal CPython fork.

--- bedevere/__main__.py
+++ bedevere/__main__.py
@@ -10,6 +10,7 @@ import cachetools
 from gidgethub import aiohttp as gh_aiohttp
 from gidgethub import routing
 from gidgethub import sansio
+from gidgethub import apps
 
 from . import backport, gh_issue, close_pr, filepaths, news, stage
 
@@ -35,6 +36,17 @@ async def main(request):
             gh = gh_aiohttp.GitHubAPI(session, "python/bedevere",
                                       oauth_token=oauth_token,
                                       cache=cache)
+
+            if event.data.get("installation"):
+                # This path only works on GitHub App
+                installation_id = event.data["installation"]["id"]
+                installation_access_token = await apps.get_installation_access_token(
+                    gh,
+                    installation_id=installation_id,
+                    app_id=os.environ.get("GH_APP_ID"),
+                    private_key=os.environ.get("GH_PRIVATE_KEY")
+                )
+                gh.oauth_token = installation_access_token["token"]
             # Give GitHub some time to reach internal consistency.
             await asyncio.sleep(1)
             await router.dispatch(event, gh, session=session)
@@ -46,6 +58,12 @@ async def main(request):
     except Exception as exc:
         traceback.print_exc(file=sys.stderr)
         return web.Response(status=500)
+
+
+@router.register("installation", action="created")
+async def repo_installation_added(event, gh, *args, **kwargs):
+    # installation_id = event.data["installation"]["id"]
+    print(f"App installed by {event.data['installation']['account']['login']}, installation_id: {event.data['installation']['id']}")
 
 
 if __name__ == "__main__":  # pragma: no cover
--- tests/test___main__.py
+++ tests/test___main__.py
@@ -1,7 +1,20 @@
 from aiohttp import web
-import pytest
+
+from unittest import mock
+
 
 from bedevere import __main__ as main
+
+from gidgethub import sansio
+
+
+app_installation_payload = {
+    "installation":
+        {
+            "id": 123,
+            "account": {"login": "mariatta"},
+        }
+    }
 
 
 async def test_ping(aiohttp_client):
@@ -36,3 +49,40 @@ async def test_failure(aiohttp_client):
     # Missing key headers.
     response = await client.post("/", headers={})
     assert response.status == 500
+
+
+@mock.patch("gidgethub.apps.get_installation_access_token")
+async def test_success_with_installation(get_access_token_mock, aiohttp_client):
+
+    get_access_token_mock.return_value = {'token': 'ghs_blablabla', 'expires_at': '2023-06-14T19:02:50Z'}
+    app = web.Application()
+    app.router.add_post("/", main.main)
+    client = await aiohttp_client(app)
+    headers = {"x-github-event": "project",
+               "x-github-delivery": "1234"}
+    # Sending a payload that shouldn't trigger any networking, but no errors
+    # either.
+    data = {"action": "created"}
+    data.update(app_installation_payload)
+    response = await client.post("/", headers=headers, json=data)
+    assert response.status == 200
+
+
+class FakeGH:
+
+    def __init__(self):
+        pass
+
+
+async def test_repo_installation_added(capfd):
+    event_data = {
+        "action": "created",
+    }
+    event_data.update(app_installation_payload)
+
+    event = sansio.Event(event_data, event='installation',
+                        delivery_id='1')
+    gh = FakeGH()
+    await main.router.dispatch(event, gh)
+    out, err = capfd.readouterr()
+    assert f"App installed by {event.data['installation']['account']['login']}, installation_id: {event.data['installation']['id']}" in out
